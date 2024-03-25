31f8cbdc58ffd138903e08a6164f66392d31540a
JKupzig <jenny.kupzig@posteo.de>
2024-03-25 15:44:25 +0100

    adding test for evaporaton form lake

--- tests/testthat/test-routing.r
+++ tests/testthat/test-routing.r
@@ -1,6 +1,6 @@
-# devtools::load_all()
+#devtools::load_all()
 
-testthat::test_that("daily soil routine",
+testthat::test_that("normal routing routine",
 {
   model <- readRDS("example_basin.rds") # ./tests/testthat/example_basin.rds
   initModel(model)
@@ -22,5 +22,29 @@ testthat::test_that("daily soil routine",
                             Prec)
   
   expect_equal(sum(routing_result$River$Discharge), 31.023392)
+})
+
+testthat::test_that("routing with evaporation from river routine",
+{
+  model <- readRDS("example_basin.rds") # ./tests/testthat/example_basin.rds
+  initModel(model)
+  Settings <- init.settings(evaporation_from_river = "on")
+  defSettings(Settings);
+  initializeModel();
+  
+  array_size <- model$array_size
+  SimPeriod <- model$SimPeriod[1:20]
+  surfaceRunoff <- matrix(1, nrow=length(SimPeriod), ncol=array_size)
+  GroundwaterRunoff <-  matrix(1, nrow=length(SimPeriod), ncol=array_size)
+  PETw <- matrix(3, nrow=length(SimPeriod), ncol=array_size)
+  Prec <- matrix(1, nrow=length(SimPeriod), ncol=array_size)
+  
+  routing_result <- routing(SimPeriod, 
+                            surfaceRunoff, 
+                            GroundwaterRunoff, 
+                            PETw, 
+                            Prec)
+  
+  expect_equal(sum(routing_result$River$Discharge), 18.3746)
 })
                       
