f15267b055ad8fd1a0a511cda00aa7e63fa51389
JKupzig <jenny.kupzig@posteo.de>
2024-03-25 11:03:01 +0100

    making functin public

--- src/initializeModel.cpp
+++ src/initializeModel.cpp
@@ -9,7 +9,7 @@ using namespace std;
 NumericVector G_PETnetShort;
 NumericVector G_PETnetLong;
 
-NumericVector daily_prec_to_soil; 
+NumericVector daily_prec_to_soil;
 NumericVector dailySoilPET; //left energy for evaporation from soil (PET)
 NumericVector dailyCanopyEvapo;
 NumericVector dailySnowMelt; //Snowmelt (flux) per day
@@ -20,12 +20,12 @@ NumericVector immediate_runoff; //Water amount that is transformed directly to s
 NumericVector dailyAET; // actual evaporation form soil
 NumericVector daily_runoff; //amount of sealed ares in grid [-]
 NumericVector soil_water_overflow; //amount of sealed ares in grid [-]
-NumericVector daily_gw_recharge; 
+NumericVector daily_gw_recharge;
 NumericVector G_dailyLocalSurfaceRunoff;
 NumericVector G_dailyLocalGWRunoff;
 NumericVector G_dailyUseGW;
 
-//initiliazing storages 
+//initiliazing storages
 NumericVector G_canopyWaterContent; //canopy storage is defined (0 content)
 NumericVector G_snow; //Snow storage for every cell and per day
 NumericMatrix G_snowWaterEquivalent; //Snow storage for every subgrid cell and per day
@@ -34,16 +34,16 @@ NumericVector G_groundwater; // groundwater storage
 
 //Creating help vectors to define routing order
 IntegerVector cellIDs; //cellIDs for actual routing step
-IntegerVector numbers; //routing steps 
-IntegerVector numbersSorted; 
-	
+IntegerVector numbers; //routing steps
+IntegerVector numbersSorted;
+
 // ROUTING
 
 //Creating working vectors
 NumericVector G_riverOutflow; // only for routing, needs ot be set to zero for every day
-NumericVector QA_river; //has always river outflow from previous time step 
-NumericVector S_river; //has always river inflow from previous time step 
-	
+NumericVector QA_river; //has always river outflow from previous time step
+NumericVector S_river; //has always river inflow from previous time step
+
 NumericVector locLake_overflow;
 NumericVector locLake_outflow;
 NumericVector S_locLakeStorage;
@@ -74,19 +74,21 @@ NumericVector S_gloWetlandStorage;
 NumericVector gloWetland_evapo;
 NumericVector gloWetland_inflow;
 
-NumericMatrix dailyUse; 
+NumericMatrix dailyUse;
 NumericVector G_totalUnsatisfiedUse;
 NumericVector G_actualUse;
-	
-	
+
+
 //' @title Initializing of model
 //' @description Vectors and Matrices are initiliazed with the appropiate size for basin (all entries are 0)
+//' @export
+// [[Rcpp::export]]
 void initializeModel(){
-	
+
 	G_PETnetShort=NumericVector(array_size);
 	G_PETnetLong=NumericVector(array_size);
 
-	daily_prec_to_soil=NumericVector (array_size); 
+	daily_prec_to_soil=NumericVector (array_size);
 	dailySoilPET=NumericVector (array_size); //left energy for evaporation from soil (PET)
 	dailyCanopyEvapo=NumericVector (array_size);
 	dailySnowMelt=NumericVector (array_size); //Snowmelt (flux) per day
@@ -97,32 +99,32 @@ void initializeModel(){
 	dailyAET=NumericVector (array_size); // actual evaporation form soil
 	daily_runoff=NumericVector (array_size); //amount of sealed ares in grid [-]
 	soil_water_overflow=NumericVector (array_size); //amount of sealed ares in grid [-]
-	daily_gw_recharge=NumericVector (array_size); 
+	daily_gw_recharge=NumericVector (array_size);
 	G_dailyLocalSurfaceRunoff=NumericVector (array_size);
 	G_dailyLocalGWRunoff=NumericVector (array_size);
 	G_dailyUseGW=NumericVector (array_size);
 
-	//initiliazing storages 
+	//initiliazing storages
 	G_canopyWaterContent=NumericVector (array_size); //canopy storage is defined (0 content)
 	G_snow=NumericVector (array_size); //Snow storage for every cell and per day
 	G_snowWaterEquivalent=NumericMatrix (25, array_size); //Snow storage for every subgrid cell and per day
 	G_soilWaterContent=NumericVector (array_size); //soil storage
 	G_groundwater=NumericVector (array_size); // groundwater storage
-	
-	
-	
+
+
+
 	//Creating help vectors to define routing order
 	IntegerVector cellIDs; //cellIDs for actual routing step
-	IntegerVector numbers; //routing steps 
-	IntegerVector numbersSorted; 
-		
+	IntegerVector numbers; //routing steps
+	IntegerVector numbersSorted;
+
 	// ROUTING
 
 	//Creating working vectors
 	G_riverOutflow=NumericVector (array_size); // only for routing, needs ot be set to zero for every day
-	QA_river=NumericVector (array_size); //has always river outflow from previous time step 
-	S_river=NumericVector (array_size); //has always river inflow from previous time step 
-		
+	QA_river=NumericVector (array_size); //has always river outflow from previous time step
+	S_river=NumericVector (array_size); //has always river inflow from previous time step
+
 	locLake_overflow=NumericVector (array_size);
 	locLake_outflow=NumericVector (array_size);
 	S_locLakeStorage=NumericVector (array_size);
@@ -152,11 +154,11 @@ void initializeModel(){
 	S_gloWetlandStorage=NumericVector (array_size);
 	gloWetland_evapo=NumericVector (array_size);
 	gloWetland_inflow=NumericVector (array_size);
-	
+
 	dailyUse=NumericMatrix (2, array_size);
 	G_totalUnsatisfiedUse=NumericVector (array_size);
 	G_actualUse=NumericVector (array_size);
-	
+
 }
 
 
