e56b2df40562db646cee98d29337a78557ba126a
JKupzig <jenny.kupzig@posteo.de>
2024-09-30 13:22:46 +0200

    fix several notes in RMD CHECK

--- src/WaterGAPLite_init.c
+++ src/WaterGAPLite_init.c
@@ -20,10 +20,13 @@ extern SEXP _WaterGAPLite_dailySnow(void *, void *, void *, void *, void *, void
 extern SEXP _WaterGAPLite_defSettings(void *);
 extern SEXP _WaterGAPLite_findNumberInVector(void *, void *);
 extern SEXP _WaterGAPLite_findUniqueValues(void *);
+extern SEXP _WaterGAPLite_getLAIdaily(void *, void *, void *, void *, void *, void *, void *);
 extern SEXP _WaterGAPLite_getRiverVelocity(void *, void *, void *);
+extern SEXP _WaterGAPLite_initModel(void *);
 extern SEXP _WaterGAPLite_numberOfDaysInMonth(void *, void *);
 extern SEXP _WaterGAPLite_numberOfDaysInYear(void *);
 extern SEXP _WaterGAPLite_routing(void *, void *, void *, void *, void *);
+extern SEXP _WaterGAPLite_routingLocalWaterBodies(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern SEXP _WaterGAPLite_routingRiver(void *, void *, void *, void *, void *);
 extern SEXP _WaterGAPLite_runModel(void *, void *, void *, void *);
 extern SEXP _WaterGAPLite_setLakeWetlandToMaximum(void *, void *, void *, void *, void *);
@@ -36,33 +39,36 @@ extern SEXP _WaterGAPLite_WaterUseCalcMeanDemandDaily(void *, void *);
 extern SEXP _WaterGAPLite_WaterUseConsumGW(void *, void *, void *);
 
 static const R_CallMethodDef CallEntries[] = {
-  {"_WaterGAPLite_CheckResType",                (DL_FUNC) &_WaterGAPLite_CheckResType,                0},
-  {"_WaterGAPLite_createWaterBalance",          (DL_FUNC) &_WaterGAPLite_createWaterBalance,          1},
-  {"_WaterGAPLite_dailyEstimateLongwave",       (DL_FUNC) &_WaterGAPLite_dailyEstimateLongwave,       4},
-  {"_WaterGAPLite_dailyEstimateShortwave",      (DL_FUNC) &_WaterGAPLite_dailyEstimateShortwave,      5},
-  {"_WaterGAPLite_dailySnow",                   (DL_FUNC) &_WaterGAPLite_dailySnow,                   9},
-  {"_WaterGAPLite_defSettings",                 (DL_FUNC) &_WaterGAPLite_defSettings,                 1},
-  {"_WaterGAPLite_findNumberInVector",          (DL_FUNC) &_WaterGAPLite_findNumberInVector,          2},
-  {"_WaterGAPLite_findUniqueValues",            (DL_FUNC) &_WaterGAPLite_findUniqueValues,            1},
-  {"_WaterGAPLite_getRiverVelocity",            (DL_FUNC) &_WaterGAPLite_getRiverVelocity,            3},
-  {"_WaterGAPLite_numberOfDaysInMonth",         (DL_FUNC) &_WaterGAPLite_numberOfDaysInMonth,         2},
-  {"_WaterGAPLite_numberOfDaysInYear",          (DL_FUNC) &_WaterGAPLite_numberOfDaysInYear,          1},
-  {"_WaterGAPLite_routing",                     (DL_FUNC) &_WaterGAPLite_routing,                     5},
-  {"_WaterGAPLite_routingRiver",                (DL_FUNC) &_WaterGAPLite_routingRiver,                5},
-  {"_WaterGAPLite_runModel",                    (DL_FUNC) &_WaterGAPLite_runModel,                    4},
-  {"_WaterGAPLite_setLakeWetlandToMaximum",     (DL_FUNC) &_WaterGAPLite_setLakeWetlandToMaximum,     5},
-  {"_WaterGAPLite_sortIt",                      (DL_FUNC) &_WaterGAPLite_sortIt,                      1},
-  {"_WaterGAPLite_sumVector",                   (DL_FUNC) &_WaterGAPLite_sumVector,                   1},
-  {"_WaterGAPLite_tools_DefDrainageCells",      (DL_FUNC) &_WaterGAPLite_tools_DefDrainageCells,      3},
-  {"_WaterGAPLite_tools_interpolate",           (DL_FUNC) &_WaterGAPLite_tools_interpolate,           3},
-  {"_WaterGAPLite_WaterUseCalcDaily",           (DL_FUNC) &_WaterGAPLite_WaterUseCalcDaily,           8},
-  {"_WaterGAPLite_WaterUseCalcMeanDemandDaily", (DL_FUNC) &_WaterGAPLite_WaterUseCalcMeanDemandDaily, 2},
-  {"_WaterGAPLite_WaterUseConsumGW",            (DL_FUNC) &_WaterGAPLite_WaterUseConsumGW,            3},
-  {NULL, NULL, 0}
+    {"_WaterGAPLite_CheckResType",                (DL_FUNC) &_WaterGAPLite_CheckResType,                 0},
+    {"_WaterGAPLite_createWaterBalance",          (DL_FUNC) &_WaterGAPLite_createWaterBalance,           1},
+    {"_WaterGAPLite_dailyEstimateLongwave",       (DL_FUNC) &_WaterGAPLite_dailyEstimateLongwave,        4},
+    {"_WaterGAPLite_dailyEstimateShortwave",      (DL_FUNC) &_WaterGAPLite_dailyEstimateShortwave,       5},
+    {"_WaterGAPLite_dailySnow",                   (DL_FUNC) &_WaterGAPLite_dailySnow,                    9},
+    {"_WaterGAPLite_defSettings",                 (DL_FUNC) &_WaterGAPLite_defSettings,                  1},
+    {"_WaterGAPLite_findNumberInVector",          (DL_FUNC) &_WaterGAPLite_findNumberInVector,           2},
+    {"_WaterGAPLite_findUniqueValues",            (DL_FUNC) &_WaterGAPLite_findUniqueValues,             1},
+    {"_WaterGAPLite_getLAIdaily",                 (DL_FUNC) &_WaterGAPLite_getLAIdaily,                  7},
+    {"_WaterGAPLite_getRiverVelocity",            (DL_FUNC) &_WaterGAPLite_getRiverVelocity,             3},
+    {"_WaterGAPLite_initModel",                   (DL_FUNC) &_WaterGAPLite_initModel,                    1},
+    {"_WaterGAPLite_numberOfDaysInMonth",         (DL_FUNC) &_WaterGAPLite_numberOfDaysInMonth,          2},
+    {"_WaterGAPLite_numberOfDaysInYear",          (DL_FUNC) &_WaterGAPLite_numberOfDaysInYear,           1},
+    {"_WaterGAPLite_routing",                     (DL_FUNC) &_WaterGAPLite_routing,                      5},
+    {"_WaterGAPLite_routingLocalWaterBodies",     (DL_FUNC) &_WaterGAPLite_routingLocalWaterBodies,     18},
+    {"_WaterGAPLite_routingRiver",                (DL_FUNC) &_WaterGAPLite_routingRiver,                 5},
+    {"_WaterGAPLite_runModel",                    (DL_FUNC) &_WaterGAPLite_runModel,                     4},
+    {"_WaterGAPLite_setLakeWetlandToMaximum",     (DL_FUNC) &_WaterGAPLite_setLakeWetlandToMaximum,      5},
+    {"_WaterGAPLite_sortIt",                      (DL_FUNC) &_WaterGAPLite_sortIt,                       1},
+    {"_WaterGAPLite_sumVector",                   (DL_FUNC) &_WaterGAPLite_sumVector,                    1},
+    {"_WaterGAPLite_tools_DefDrainageCells",      (DL_FUNC) &_WaterGAPLite_tools_DefDrainageCells,       3},
+    {"_WaterGAPLite_tools_interpolate",           (DL_FUNC) &_WaterGAPLite_tools_interpolate,            3},
+    {"_WaterGAPLite_WaterUseCalcDaily",           (DL_FUNC) &_WaterGAPLite_WaterUseCalcDaily,            8},
+    {"_WaterGAPLite_WaterUseCalcMeanDemandDaily", (DL_FUNC) &_WaterGAPLite_WaterUseCalcMeanDemandDaily,  2},
+    {"_WaterGAPLite_WaterUseConsumGW",            (DL_FUNC) &_WaterGAPLite_WaterUseConsumGW,             3},
+    {NULL, NULL, 0}
 };
 
 void R_init_WaterGAPLite(DllInfo *dll)
 {
-  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-  R_useDynamicSymbols(dll, FALSE);
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
 }
